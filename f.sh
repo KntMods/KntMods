90c298a1e46be67c05c96f934e33e27045c30f88
Kanetav <86632945+KntMods@users.noreply.github.com>
2024-03-04 04:42:40 +0700

    Update f.sh

--- f.sh
+++ f.sh
@@ -3,5 +3,5 @@
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 source ~/.bashrc   
 nvm install --lts 
-sudo apt install nginx
 nvm alias default v20.11.1
+sudo apt install nginx -y
